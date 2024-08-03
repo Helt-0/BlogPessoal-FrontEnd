3bf207b9a3e95cdf6126f66db476426303b0f11b
lucas silva <lucas.vasconcelosop@hotmail.com>
2024-08-02 21:52:38 -0300

    :tada: First commit (configurando o projeto)

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+
+    return (
+        <>
+            <h1 className="titulo">Home </h1>
+
+            <img src="https://i.imgur.com/azdJUuT.jpeg" alt="Paisagem Tela inicial" className="img" />
+
+        </>
+    );
+
+}
+
+export default Home;
