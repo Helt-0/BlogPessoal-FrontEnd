1e0661433537ea5d2b4d626dd95e829e00385afe
lucas silva <lucas.vasconcelosop@hotmail.com>
2024-08-12 20:54:09 -0300

    :construction: Construindo e implementando o CRUD de Temas

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,26 @@ import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/cadastro';
 import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
-
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTemas/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
     <>
-     <AuthProvider>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Login/>} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
