d5239ecad82b337d30d7365860afbafd83b52dc2
lucas silva <lucas.vasconcelosop@hotmail.com>
2024-08-14 12:43:10 -0300

    :construction: CRUD de postagens

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,17 @@ import Cadastro from './pages/cadastro/cadastro';
 import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
-import FormularioTema from './components/temas/formularioTemas/FormularioTema';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens.tsx';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+
 
 function App() {
   return (
     <>
-      <AuthProvider>
+    <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
@@ -28,11 +32,15 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-      </AuthProvider>
+        </AuthProvider>
     </>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import homeLogo from '../../assets/home.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
+
 
 function Home() {
     return (
@@ -12,9 +16,9 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
@@ -23,7 +27,7 @@ function Home() {
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }
